a6057149b88a59a3a893654f4c816af030dc368b
Amit Patil <amitbpatil2004@gmail.com>
2025-08-12 23:52:20 +0530

    Added custom signup and signin pages

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,10 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+  return (
+    <SignUp/>  // This will render the sign-up form provided by Clerk
+  )
+}
+
+export default page
