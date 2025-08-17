d43e92158149136473cb1aa82e1485ba4e822b7c
Amit Patil <amitbpatil2004@gmail.com>
2025-08-17 16:47:14 +0530

    Created the background inngest function which runs automatically

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../lib/inngest/client";
-import { helloWorld } from "../../../lib/inngest/functions";
+import { generateIndustryInsights } from "../../../lib/inngest/functions";
+
 
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-     // <-- This is where you'll always add all your functions
-  ],
+  functions: [generateIndustryInsights],
 });
